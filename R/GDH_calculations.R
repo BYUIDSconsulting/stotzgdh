a4e19355dee3bfa923cb92983b992ff9fdea8525
Mike-Min <min19001@byui.edu>
2023-03-09 21:20:00 -0700

    Update GDH_calculations.R

--- R/GDH_calculations.R
+++ R/GDH_calculations.R
@@ -1,4 +1,4 @@
-# For all functions related to GDH calculations
+# For all functions related to GDH (Growth Degree Hour) calculations
 
 
 
@@ -7,24 +7,28 @@
 #' @description
 #' Gets the summation of the growing degree hours for a specific crop.
 #'
-#' @param ...
+#' @param ... The 24 hour temperature info
 #' @param crop The name of the crop
-#' @param crop_data The specific growing degree range. So the Minimum and Maximum degrees
+#' @param crop_data The specific growing degree range. So the Minimum and Maximum degrees. Mike-edit: The file should contain base temperature and upper threshold temperature of each crop in F
 #'
-#' @return A numeric value
+#' @return A numeric value, the GDD--Growth Degree Day for a specific crop
 #' @export
 #'
 #' @examples
 gdh <- function(..., crop, crop_data) {
   crop_subset <- subset(crop_data, crop_name == crop)
-  base_temp <- crop_subset[1,2]
-
-  # gd <- ... - base_temp
-
-  # if( gd <  0 ){gd <- 0}
-
-  # Replace negative values with zero using pmax()
-  values <- pmax(... - base_temp, 0)
-  # Take the sum of the values
-  return(sum(values))
+  # Obtain base temp value from crop_data based on crop
+  base_temp <- as.numeric(crop_subset[1,2])
+  # Obtain upper threshold value from crop_data based on crop
+  threshold <- as.numeric(crop_subset[1,4])
+  # Check for NA values using is.na() and return NA if any temperature values are missing
+  if (any(is.na(c(...)))) {
+    return(NA)
+  } else {
+    # Replace negative values with zero using pmax()
+    # Replace values exceeding the threshold with the threshold value
+    values <- pmax(pmin(..., threshold)-base_temp, 0)
+    # Take the sum of the values, hence summing 24 GDH values into a GDD value
+    return(sum(values))
+  }
 }
